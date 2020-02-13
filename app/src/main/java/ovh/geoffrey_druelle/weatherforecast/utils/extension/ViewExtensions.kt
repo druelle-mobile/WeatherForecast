681dbd3983de82c4064152df1938829dc03af07f
Geoffrey Druelle <gdruelle@timcod.fr>
2020-02-13 14:39:59 +0100

    Adds keyboard hiding ext

--- app/src/main/java/ovh/geoffrey_druelle/weatherforecast/utils/extension/ViewExtensions.kt
+++ app/src/main/java/ovh/geoffrey_druelle/weatherforecast/utils/extension/ViewExtensions.kt
@@ -1,8 +1,11 @@
 package ovh.geoffrey_druelle.weatherforecast.utils.extension
 
+import android.app.Activity
 import android.view.View
 import android.view.View.GONE
 import android.view.View.VISIBLE
+import android.view.inputmethod.InputMethodManager
+import ovh.geoffrey_druelle.weatherforecast.ui.main.MainActivity
 
 fun View.show() {
     visibility = VISIBLE
@@ -10,4 +13,25 @@ fun View.show() {
 
 fun View.hide() {
     visibility = GONE
+}
+
+fun hideKeyboard(view: View?) {
+    if (view == null) {
+        return
+    }
+    val activity = MainActivity.instance
+    val imm = activity.getSystemService(
+        Activity.INPUT_METHOD_SERVICE
+    ) as InputMethodManager
+    imm.hideSoftInputFromWindow(view.windowToken, 0)
+}
+
+fun hideKeyboard(activity: Activity) {
+    val imm =
+        activity.getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
+    var view = activity.currentFocus
+    if (view == null) {
+        view = View(activity)
+    }
+    imm.hideSoftInputFromWindow(view.windowToken, 0)
 }
